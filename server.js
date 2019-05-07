7dccc844c6c3dd382cd6793ab53c6ff2d80df833
Michael Olsen <michaelolsen@michaels-air.uconnect.utah.edu>
2019-05-06 21:45:14 -0600

    period

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require( "express" );
 const path = require( "path" );
 
 var app = express();
-var PORT = process.envPORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use( express.urlencoded( { extended: true } ) )
 app.use( express.json() );
